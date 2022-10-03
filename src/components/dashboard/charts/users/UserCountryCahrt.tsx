34ea9ea32f1d780458f4e1938b64bf30e4dc4d3c
BogdanDomashenko <got.bogdan@gmail.com>
2022-10-03 16:43:55 +0300

    first commit

--- src/components/dashboard/charts/users/UserCountryCahrt.tsx
+++ src/components/dashboard/charts/users/UserCountryCahrt.tsx
@@ -14,6 +14,12 @@ interface Props {
   countries: CountryPopularity[];
 }
 
+const COLORS: string[] = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const renderLabel = (country: CountryPopularity): string => {
+  return country.name;
+};
+
 export const UserCountryChart: FC<Props> = ({ countries }) => {
   return (
     <ResponsiveContainer width="100%" height={200}>
@@ -22,12 +28,16 @@ export const UserCountryChart: FC<Props> = ({ countries }) => {
           dataKey="count"
           isAnimationActive={false}
           data={countries}
+          label={renderLabel}
           cx="50%"
           cy="50%"
           outerRadius={80}
           fill="#8884d8"
-          label
-        />
+        >
+          {countries.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
         <Tooltip />
       </PieChart>
     </ResponsiveContainer>
